c9e83130a1c893b37253c6756d459069bb434189
Michael Eckert <meckert@salesforce.com>
2017-05-15 13:36:23 +0200

    removed everything except beta, added rounding and right-justification

--- static/index.js
+++ static/index.js
@@ -5,10 +5,8 @@ app.controller('IndexCtrl', function ($scope, $http, $timeout) {
     window.addEventListener("deviceorientation", handleOrientation, true);
     function handleOrientation(event) {
         $scope.$apply(function(){
-            $scope.absolute = event.absolute;
-            $scope.alpha    = event.alpha;
-            $scope.beta     = event.beta;
-            $scope.gamma    = event.gamma;
+
+            $scope.beta = Math.round(event.beta);
         });
     }
 });
