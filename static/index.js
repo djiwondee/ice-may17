225d2149ba911712c13a992c7575fd8174eac6a2
Michael Eckert <meckert@salesforce.com>
2017-05-15 13:52:39 +0200

    added slider etc.

--- static/index.js
+++ static/index.js
@@ -1,11 +1,9 @@
 var app = angular.module('Index', []);
 
 app.controller('IndexCtrl', function ($scope, $http, $timeout) {
-
     window.addEventListener("deviceorientation", handleOrientation, true);
     function handleOrientation(event) {
         $scope.$apply(function(){
-
             $scope.beta = Math.round(event.beta);
         });
     }
