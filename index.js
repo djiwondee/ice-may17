443fd13644720693855eb698e5ae510bc3bc8187
Michael Eckert <meckert@salesforce.com>
2017-05-15 18:54:26 +0200

    Started to add case opening functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
+var Promise = require('bluebird');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -16,37 +17,52 @@ var sf_user = process.env.SF_USER;
 var sf_pass = process.env.SF_PASS;
 
 app.get('/', function(req, res) {
-    res.redirect(301, '/chart-tilt');    
+    res.redirect(301, '/chart-tilt');
 });
 
 app.post('/', function(req, res) {
     res.redirect(301, '/chart-tilt');
-    /*
-    var options = {
-        root: __dirname + '/static/',
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    };
-    res.sendFile('index.html', options, function (err) {
-        if (err) {
-            console.log(err);
-            res.status(err.status).end();
-        }
-    });
-    */
 });
 
-app.get('/temperature', function(req, res) {
-    res.send({
-        "temperature": temperature,
-        "buttonPressed": false
+var org = null;
+app.post('/flic-button', function(req, res) {
+    if(!org) {
+        res.status(500);
+        return;
+    }
+
+    var c = nforce.createSObject('Case');
+    //c.set('AssetId', process.env.CASE_ASSETID);
+    c.set('Subject', process.env.CASE_SUBJECT);
+    c.set('SuppliedEmail', process.env.CASE_SUPPLIEDEMAIL);
+
+    org.insert({sobject: c})
+    .then(function(result){
+        console.log("Sucess");
+        res.status(200).send("OK");
+    })
+    .catch(function(err){
+        res.status(503);
     });
 });
 
-
+if(sf_user != "CHANGE ME" && sf_pass != "CHANGE ME") {
+    org = nforce.createConnection({
+        clientId: '3MVG9HxRZv05HarRXTda.1RkrphmmT6POMnpBcXWt1.nGo2SFu4kKSRKdMgm2JiLWb71uU8Q3QJDVH6SNlh.C',
+        clientSecret: '1697918172555568564',
+        redirectUri: '',
+        mode: 'single',
+        plugins: []
+    });
+    org.authenticate({ username: sf_user, password: sf_pass})
+    .then(function(){
+        console.log("Authenicated successfully against Salesforce Org");
+    })
+    .catch(function(err){
+        console.log("Error authenicating against Salesforce Org");
+        console.log(err);
+    });
+}
 
 var server = http.createServer(app);
 server.listen(port);
@@ -64,24 +80,6 @@ wss.on("connection", function(ws) {
             }
         });
     });
-
-    /*
-    var tilt = 0;
-    var id = setInterval(newTilt, 1000);
-    function newTilt() {
-        var delta = Math.random() * 60 - 30;
-        if(tilt + delta > 180 || tilt + delta < -180) {
-            delta = -delta;
-        }
-        tilt = tilt + delta;
-        var data = {
-            timestamp: Date.now(),
-            tilt: tilt
-        };
-        ws.send(JSON.stringify(data));
-    };
-    console.log("websocket connection open");
-    */
 
     ws.on("close", function() {
         console.log("websocket connection close");
